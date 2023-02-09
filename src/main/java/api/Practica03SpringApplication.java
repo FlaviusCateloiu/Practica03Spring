52383b36dbfde72ba82d7586ae4554d6c326e0e9
flaars <fcateloiu08@gmail.com>
2023-02-09 19:45:20 +0100

    Initial commit

---
+++ src/main/java/api/Practica03SpringApplication.java
@@ -0,0 +1,13 @@
+package api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica03SpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica03SpringApplication.class, args);
+	}
+
+}
