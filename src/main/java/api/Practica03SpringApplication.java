73a100ae0dcf8fce587ca6372583ef8db7478f7b
flaars <fcateloiu08@gmail.com>
2023-02-09 19:45:42 +0100

    He hecho hasta ahora lo que tengo que hacer.

--- src/main/java/api/Practica03SpringApplication.java
+++ src/main/java/api/Practica03SpringApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Practica03SpringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Practica03SpringApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Practica03SpringApplication.class, args);
+    }
 
 }
--- src/test/java/api/Practica03SpringApplicationTests.java
+++ src/test/java/api/Practica03SpringApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Practica03SpringApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
